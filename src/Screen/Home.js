8e2541dde039fb7002a242fcae7db48f6fa0531a
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-09 03:45:21 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import './Style/Home.css';
+import React from "react";
+import "./Style/Home.css";
 
+import { Button, Space } from "antd";
+import {
+  GiftOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  MessageOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="homeBody">
+      <div className="homeTopBox"></div>
+      <div className="homeMedBox">akash</div>
+      {/* BottomBox */}
+      <div className="homeBottomBox">
+        <Button type="link" block className="homeBottomBoxButton1">
+          <GiftOutlined className="homeBottomBoxButtonIcon" />
+          <p className="homeBottomBoxButtonText">Offers</p>
+        </Button>
+
+        <Button type="link" block className="homeBottomBoxButton2">
+          <ShoppingCartOutlined className="homeBottomBoxButtonIcon" />
+          <p className="homeBottomBoxButtonText">Cart</p>
+        </Button>
+
+        <Button type="link" block className="homeBottomBoxButton2">
+          <ShoppingOutlined className="homeBottomBoxButtonIcon" />
+          <p className="homeBottomBoxButtonText">Pre-Order</p>
+        </Button>
+
+        <Button type="link" block className="homeBottomBoxButton2">
+          <MessageOutlined className="homeBottomBoxButtonIcon" />
+          <p className="homeBottomBoxButtonText">Contacts</p>
+        </Button>
+
+        <Button type="link" block className="homeBottomBoxButton2">
+          <UserOutlined className="homeBottomBoxButtonIcon" />
+          <p className="homeBottomBoxButtonText">Account</p>
+        </Button>
+      </div>
+    </div>
+  );
 }
