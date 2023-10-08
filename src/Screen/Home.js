280ce3795b7e9d81939fa940bc33108c46ed49ca
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-09 04:40:25 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,21 +1,68 @@
 import React from "react";
 import "./Style/Home.css";
 
-import { Button, Space } from "antd";
+import { Button, Space, Input } from "antd";
 import {
   GiftOutlined,
   ShoppingCartOutlined,
   ShoppingOutlined,
   MessageOutlined,
   UserOutlined,
+  AudioOutlined,
+  PicCenterOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
+
+const { Search } = Input;
+
+const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 export default function Home() {
   return (
     <div className="homeBody">
-      {/* BottomBox */}
-      <div className="homeTopBox"></div>
-      {/* BottomBox */}
+      {/* TopBox */}
+      <div className="homeTopBox">
+      <Button  className="homeTopBoxMenu" shape="circle" size="large" icon={<PicCenterOutlined/>} />
+        <h1 style={{ color: "#ffffff", flex: "1" }}>Logo&Name</h1>
+        <Button  className="homeTopBoxSearch" shape="circle" size="large" icon={<SearchOutlined />} />
+        <div className="homeTopBoxSearchDiv">
+          <Search
+            placeholder="Search"
+            onSearch={onSearch}
+            style={{
+              width: 250,
+            }}
+            addonAfter={<AudioOutlined />}
+          />
+        </div>
+        <div className="homeTopBoxLeftDiv">
+          <Button type="link" block className="homeBottomBoxButton1">
+            <GiftOutlined className="homeBottomBoxButtonIcon" />
+            <p className="homeBottomBoxButtonText">Offers</p>
+          </Button>
+
+          <Button type="link" block className="homeBottomBoxButton2">
+            <ShoppingCartOutlined className="homeBottomBoxButtonIcon" />
+            <p className="homeBottomBoxButtonText">Cart</p>
+          </Button>
+
+          <Button type="link" block className="homeBottomBoxButton2">
+            <ShoppingOutlined className="homeBottomBoxButtonIcon" />
+            <p className="homeBottomBoxButtonText">Pre-Order</p>
+          </Button>
+
+          <Button type="link" block className="homeBottomBoxButton2">
+            <MessageOutlined className="homeBottomBoxButtonIcon" />
+            <p className="homeBottomBoxButtonText">Contacts</p>
+          </Button>
+
+          <Button type="link" block className="homeBottomBoxButton2">
+            <UserOutlined className="homeBottomBoxButtonIcon" />
+            <p className="homeBottomBoxButtonText">Account</p>
+          </Button>
+        </div>
+      </div>
+      {/* MidBox */}
       <div className="homeMedBox">akash</div>
       {/* BottomBox */}
       <div className="homeBottomBox">
