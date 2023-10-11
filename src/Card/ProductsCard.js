cefc2647abf549943974a7e94c5d8784214648c9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-11 21:46:44 +0600

    ok

--- src/Card/ProductsCard.js
+++ src/Card/ProductsCard.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from "react";
 import "./ProductsCard.css";
 
-import { Image, Button } from 'antd';
+import { Image, Button } from "antd";
 
-import walletMen from '../Image/Leather wallet for men.jpeg'
+import walletMen from "../Image/Leather wallet for men.jpeg";
+import taka from "../Icon/taka (1).png";
+import taka2 from "../Icon/taka.png";
 
 export default function ProductsCard() {
   return (
-    
-    <Button className='ProductsCardBody'>
-            <Image height={170} width='90%' className='ProductsCardImage' src={walletMen}/>
-            <div className='ProductsCardTextDiv'>
-                <p className='ProductsCardText'>High quality Artificial <br/> Leather wallet for men</p>
-            </div>
-            <div className='ProductsCardBox'>
-                <p className='ProductsCardPrice'>115 TK</p>
-            </div>
+    <Button className="ProductsCardBody">
+      <Image
+        height={170}
+        width="90%"
+        className="ProductsCardImage"
+        src={walletMen}
+      />
+      <div className="ProductsCardTextDiv">
+        <p className="ProductsCardText">
+          High quality Artificial <br /> Leather wallet for men
+        </p>
+      </div>
+      <div className="ProductsCardBox">
+        <div style={{display:'flex'}}>
+          <img height={15} width={15} className="ProductsCardPriceIcon" src={taka} />
+          <p className="ProductsCardPrice">115</p>
+        </div>
+        <div style={{display:'flex', marginTop:'-25px'}}>
+          <img height={13} width={13} className="ProductsCardPriceIcon2" src={taka2} />
+          <p className="ProductsCardPrice2">115</p>
+          <p className="ProductsCardPrice3">-20%</p>
+        </div>
 
+      </div>
     </Button>
-  )
+  );
 }
