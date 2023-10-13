2f9ccd41714f49df08fb4b7a14cc197616a95f3e
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-14 01:02:17 +0600

    ok

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import './Title.css';
+import React from "react";
+import "./Title.css";
 import { Button, Flex } from "antd";
+
+import SafePayments from "../Icon/safety.png";
+import Delivery from '../Icon/food-delivery.png';
+import EasyReturns from '../Icon/return.png';
+import BestPrice from '../Icon/price-tag.png';
+import Authentic from '../Icon/authentic.png';
+import Verified from '../Icon/biometric-identification.png';
+
 
 export default function Title() {
   return (
-    <div className='titleBody'>
-        <div className='titleBox'>
-            ok
-        </div>
-         </div>
-  )
+    <Button type="text" className="titleBody">
+      <div className="titleBox">
+        <img height={25} src={SafePayments} />
+        <p className="titleText"> Safe Payments</p>
+        <div className="titleLine" />
+      </div>
+      <div className="titleBox">
+        <img height={25} src={Delivery} />
+        <p className="titleText"> Nationwide Delivery</p>
+        <div className="titleLine" />
+      </div>
+      <div className="titleBox">
+        <img height={25} src={EasyReturns} />
+        <p className="titleText"> Free & Easy Returns</p>
+        <div className="titleLine" />
+      </div>
+      <div className="titleBox">
+        <img height={25} src={BestPrice} />
+        <p className="titleText"> Best Price Guaranteed</p>
+        <div className="titleLine" />
+      </div>
+      <div className="titleBox">
+        <img height={25} src={Authentic} />
+        <p className="titleText"> 100% Authentic Products</p>
+        <div className="titleLine" />
+      </div>
+      <div className="titleBox">
+        <img height={25} src={Verified} />
+        <p className="titleText">Verified Products </p>
+      </div>
+    </Button>
+  );
 }
