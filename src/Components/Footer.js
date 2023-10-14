a0882a194dea5e03e7d932450c45a4c116175dfc
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-14 18:05:07 +0600

    ok

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+
+import { Flex, Radio,  Button } from "antd";
+
+import phoneLogo from '../Icon/phone-call.png';
+import location from '../Icon/google-maps.png'
 
 export default function Footer() {
   return (
     <div>
-        <footer className="footer">
-      <div className="container">
-        <p>&copy; {new Date().getFullYear()} Tasnim Shahriar Akash | All rights reserved</p>
+      <div className="footerFlex">
+        <div className="footerBox">
+          <p className="footerBoxText">SUPPORT</p>
+          <Button type="text" block>
+      text
+    </Button>
+          <Button type="text" block className="footerBoxButton">
+            <img height={30} src={phoneLogo}/>
+            <p className="footerNumberText"> 017** ******</p>
+          </Button>
+          <Button type="text" block className="footerBoxButton">
+            <img height={30} src={location}/>
+            <p className="footerNumberText">Map View</p>
+          </Button>
+          
+        </div>
+        <div className="footerBox">
+          <h1>hhhhh</h1>
+        </div>
+        <div className="footerBox">
+          <h1>hhhhh</h1>
+        </div>
+        <div className="footerBox">
+          <h1>hhhhh</h1>
+        </div>
       </div>
-    </footer>
+      <footer className="footer">
+        <div className="container">
+          <p>
+            &copy; {new Date().getFullYear()} Tasnim Shahriar Akash | All rights
+            reserved
+          </p>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
