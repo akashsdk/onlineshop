5ee788054ad63bc0b053ea6995e0b92d3242a942
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-14 21:30:33 +0600

    ok

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Footer.css";
 
-import { Flex, Radio,  Button } from "antd";
+import { Flex, Radio, Button } from "antd";
 
-import phoneLogo from '../Icon/phone-call.png';
-import location from '../Icon/google-maps.png'
+import phoneLogo from "../Icon/phone-call.png";
+import location from "../Icon/google-maps.png";
 
 export default function Footer() {
   return (
@@ -12,27 +12,74 @@ export default function Footer() {
       <div className="footerFlex">
         <div className="footerBox">
           <p className="footerBoxText">SUPPORT</p>
-          <Button type="text" block>
-      text
-    </Button>
           <Button type="text" block className="footerBoxButton">
-            <img height={30} src={phoneLogo}/>
+            <img height={30} src={phoneLogo} />
             <p className="footerNumberText"> 017** ******</p>
           </Button>
           <Button type="text" block className="footerBoxButton">
-            <img height={30} src={location}/>
+            <img height={30} src={location} />
             <p className="footerNumberText">Map View</p>
           </Button>
+          <div>
+            <p style={{ color: "#ffff" }}>
+              Shop & Display Center Address: <br /> <br />
+              , Dhaka-1205. Bangladesh <br />
+              (Everyday 10am-11pm) <br />
+              Mail us: <br />
+            </p>
+          </div>
+        </div>
+        <div className="footerBox">
+          <p className="footerBoxText">ABOUT US</p>
+          <div className="footerBox2Div">
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> About Us </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Careers </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Blog </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Join Our Community </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText">
+                {" "}
+                Order Tracking Delivery{" "}
+              </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Terms ﹠ Condition </p>
+            </Button>
+          </div>
+        </div>
+        <div className="footerBox">
+          <p className="footerBoxText">HELP</p>
+          <div className="footerBox2Div">
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> EMI Policy </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Privacy Policy </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Refund Policy </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Warranty Policy </p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Gift Voucher</p>
+            </Button>
+            <Button className="footerBox2DivButton" type="text" block>
+              <p className="footerBox2DivButtonText"> Complain / Advice </p>
+            </Button>
+          </div>
+        </div>
+        <div className="footerBox">
           
-        </div>
-        <div className="footerBox">
-          <h1>hhhhh</h1>
-        </div>
-        <div className="footerBox">
-          <h1>hhhhh</h1>
-        </div>
-        <div className="footerBox">
-          <h1>hhhhh</h1>
         </div>
       </div>
       <footer className="footer">
