f88032339f1ae9ca654567d40af81ccab0417ac9
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-10-16 21:49:38 +0600

    ok

--- src/Screen/Cart.js
+++ src/Screen/Cart.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import './Style/Cart.css';
-import Footer from '../Components/Footer';
+import React from "react";
+import "./Style/Cart.css";
+import Footer from "../Components/Footer";
 
 export default function Cart() {
   return (
     <div className="overflowBox">
-      <div>
-        Cart
+      <div className="cartBody">
+        <div className="cartBox">
+          <p>Shopping Cart</p>
+          <div className="cartTopBox">
+            <div className="cartTopImg">
+              <h3>Image</h3>
+            </div>
+            <div className="cartTopText">
+              <h3>Product</h3>
+            </div>
+            <div className="cartTopModel">
+              <h3>Model</h3>
+            </div>
+            <div className="cartTopQuantity">
+              <h3>Quantity</h3>
+            </div>
+            <div className="cartTopPrice">
+              <h3>Unit Price</h3>
+            </div>
+            <div className="cartTopTotal">
+              <h3>Total</h3>
+            </div>
+          </div>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
