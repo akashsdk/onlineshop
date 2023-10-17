cdd996c7d6fb2dafe7b795aabb091fd7e52ca7e0
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-18 02:53:29 +0600

    ok

---
+++ src/AccountPage/ForgetPassword.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ForgetPassword.css';
+
+export default function ForgetPassword() {
+  return (
+    <div>ForgetPassword</div>
+  )
+}
--- src/Screen/Account.js
+++ src/Screen/Account.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Style/Contacts.css";
 import Footer from "../Components/Footer";
+import Login from '../AccountPage/Login';
+import ForgetPassword from '../AccountPage/ForgetPassword';
+import Register from '../AccountPage/Register';
 
 export default function Account() {
   return (
