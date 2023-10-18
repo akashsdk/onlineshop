2d93b5a775c596b5557ee4b2f5f645577ff38958
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-18 17:51:45 +0600

    ok

--- src/Card/OffersCard.js
+++ src/Card/OffersCard.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./OffersCard.css";
 import { Button, Image } from "antd";
+import { Link } from "react-router-dom";
 
-export default function OffersCard({ percentage, number, Img, name}) {
+export default function OffersCard({ percentage, number, Img, name }) {
   const [result, setResult] = useState("");
 
   useEffect(() => {
@@ -28,29 +29,25 @@ export default function OffersCard({ percentage, number, Img, name}) {
         <p>{percentage}% Off</p>
       </div>
       <div className="OffersCardImgDiv">
-        <Image
-        height={100}
-          className="OffersCardImg"
-          src={Img}
-        />
-        <div/>
+        <Image height={100} className="OffersCardImg" src={Img} />
+        <div />
         <div className="OffersCardTextBox">
           <h3 style={{ color: "black" }}>{name}</h3>
           <div style={{ display: "flex" }}>
             <p className="OffersCardTextResult">
-              {result !== "" ? (
-                <p> {result}</p>
-              ) : (
-                <p>Enter Price</p>
-              )}
+              {result !== "" ? <p> {result}</p> : <p>Enter Price</p>}
             </p>
             <p className="OffersCardTextPrice">{number}</p>
           </div>
           <div style={{ display: "flex" }}>
-            <Button type="primary" className="OffersCardBuyButton">
-              Buy Now
-            </Button>
-            <Button className="OffersCardCartButton">Add Cart</Button>
+            <Link to="/Checkout">
+              <Button type="primary" className="OffersCardBuyButton">
+                Buy Now
+              </Button>
+            </Link>
+            <Link to="/My-Cart">
+              <Button className="OffersCardCartButton">Add Cart</Button>
+            </Link>
           </div>
         </div>
       </div>
