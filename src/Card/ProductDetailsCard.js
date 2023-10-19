fc42e45df5268143ae092747754e8b601a16a231
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-20 03:51:11 +0600

    ok

--- src/Card/ProductDetailsCard.js
+++ src/Card/ProductDetailsCard.js
@@ -14,7 +14,6 @@ import i15PromaxGold from "../Image/i15PromaxGold.jpeg";
 export default function ProductDetailsCard() {
   const [page, setPage] = useState(1);
 
-
   return (
     <div className="pDetailsCardBody">
       <div className="pDetailsCardBodyRight">
@@ -37,10 +36,20 @@ export default function ProductDetailsCard() {
             <p style={{ marginTop: "-0px" }}>28% Off</p>
           </div>
           <div className="pDetailsCardRightMid">
-            <Image
-              width={300}
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-            />
+            {page === 1 ? (
+              <div><Image width={300} height={300} src={i15PromaxGray} /></div>
+            ) : page === 2 ? (
+              <div><Image width={300} height={300} src={i15PromaxBlack} /></div>
+            ) : page === 3 ? (
+                <div><Image width={300} height={300} src={i15PromaxDarkBlue} /></div>
+              ) : page === 4 ? (
+                <div><Image width={300} height={300} src={i15PromaxWhite} /></div>
+              ) : page === 5 ? (
+                <div><Image width={300} height={300} src={i15PromaxGold} /></div>
+              ) : (
+              <h1> Error page</h1>
+            )}
+            
             <div className="pDetailsCardRightDown-2">
               <div
                 style={{
@@ -48,23 +57,63 @@ export default function ProductDetailsCard() {
                   overflowX: "scroll",
                 }}
               >
-                <Button className="pDetailsCardRightDown-2Button">
+                <Button
+                  className="pDetailsCardRightDown-2Button"
+                  onClick={() => {
+                    setPage(1);
+                  }}
+                  style={{
+                    borderColor: page === 1 ? "#fb6565" : "#ffff",
+                  }}
+                >
                   <img style={{ height: "70px" }} src={i15PromaxGray} />
                 </Button>
 
-                <Button className="pDetailsCardRightDown-2Button">
+                <Button
+                  className="pDetailsCardRightDown-2Button"
+                  onClick={() => {
+                    setPage(2);
+                  }}
+                  style={{
+                    borderColor: page === 2 ? "#fb6565" : "#ffff",
+                  }}
+                >
                   <img style={{ height: "70px" }} src={i15PromaxBlack} />
                 </Button>
 
-                <Button className="pDetailsCardRightDown-2Button">
+                <Button
+                  className="pDetailsCardRightDown-2Button"
+                  onClick={() => {
+                    setPage(3);
+                  }}
+                  style={{
+                    borderColor: page === 3 ? "#fb6565" : "#ffff",
+                  }}
+                >
                   <img style={{ height: "70px" }} src={i15PromaxDarkBlue} />
                 </Button>
 
-                <Button className="pDetailsCardRightDown-2Button">
+                <Button
+                  className="pDetailsCardRightDown-2Button"
+                  onClick={() => {
+                    setPage(4);
+                  }}
+                  style={{
+                    borderColor: page === 4 ? "#fb6565" : "#ffff",
+                  }}
+                >
                   <img style={{ height: "70px" }} src={i15PromaxWhite} />
                 </Button>
 
-                <Button className="pDetailsCardRightDown-2Button">
+                <Button
+                  className="pDetailsCardRightDown-2Button"
+                  onClick={() => {
+                    setPage(5);
+                  }}
+                  style={{
+                    borderColor: page === 5 ? "#fb6565" : "#ffff",
+                  }}
+                >
                   <img style={{ height: "70px" }} src={i15PromaxGold} />
                 </Button>
               </div>
