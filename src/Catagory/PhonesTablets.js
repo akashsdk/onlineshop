635d762e6f464bbadba4eca716c3bbf7d4a61af0
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-23 02:06:35 +0600

    ok

--- src/Catagory/PhonesTablets.js
+++ src/Catagory/PhonesTablets.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PhonesTablets.css";
 import Footer from "../Components/Footer";
 
-import { Button, Flex } from "antd";
-
-
-
+import { Button, Flex, Drawer, Select } from "antd";
+import { FilterOutlined } from "@ant-design/icons";
 
 export default function PhonesTablets() {
+  const [open, setOpen] = useState(false);
+  const [size, setSize] = useState();
+
+  const showDefaultDrawer = () => {
+    setSize("default");
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (value) => {
+    console.log(value); 
+  };
   return (
     <div className="overflowBox">
       <div style={{ height: "auto", width: "100%" }}>
@@ -18,20 +31,77 @@ export default function PhonesTablets() {
                 length: 24,
               },
               (_, i) => (
-                <Button className="phonesTabletsTopBox-Button" shape="round"  >
+                <Button className="phonesTabletsTopBox-Button" shape="round">
                   <p className="phonesTabletsTopBox-ButtonText">Download</p>
-                
-              </Button>
+                </Button>
               )
             )}
           </Flex>
         </div>
-        <div style={{ height: "auto", width: "100%", marginTop:'40px', display:'flex' }}>
+        <div
+          style={{
+            height: "auto",
+            width: "100%",
+            marginTop: "40px",
+            display: "flex",
+          }}
+        >
           <div className="phonesTabletsLeftBox">Left</div>
           <div className="phonesTabletsRightBox">
             <div className="phonesTabletsRight-Header">
-              <h3 className="phonesTabletsRight-HeaderText">Phones & Tablets</h3>
-              <h3>Sort By: </h3>
+              <h3 className="phonesTabletsRight-HeaderText">
+                Phones & Tablets
+              </h3>
+              {/* Filter */}
+              <Button
+                className="phonesTabletsRight-HeaderFilter"
+                icon={<FilterOutlined />}
+                onClick={showDefaultDrawer}
+                danger
+              >
+                Filter
+              </Button>
+
+              <Drawer
+                title="Filter"
+                placement="right"
+                size="default"
+                onClose={onClose}
+                open={open}
+              >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+              </Drawer>
+              {/* Sort By: */}
+              <div className="phonesTabletsRight-HeaderSortBy">
+                <h3>Sort By Price: </h3>
+                <Select
+                  labelInValue
+                  defaultValue={{
+                    value: "Default",
+                    label: "Default",
+                  }}
+                  style={{
+                    width: 120,
+                  }}
+                  onChange={handleChange}
+                  options={[
+                    {
+                      value: "1",
+                      label: "Default",
+                    },
+                    {
+                      value: "2",
+                      label: "Low To High",
+                    },
+                    {
+                      value: "3",
+                      label: "High to Low",
+                    },
+                  ]}
+                />
+              </div>
             </div>
           </div>
         </div>
