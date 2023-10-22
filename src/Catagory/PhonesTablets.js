8caf9a78f9738313268b5ddcd18609c667a036c8
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-23 01:16:15 +0600

    ok

--- src/Catagory/PhonesTablets.js
+++ src/Catagory/PhonesTablets.js
@@ -2,10 +2,35 @@ import React from "react";
 import "./PhonesTablets.css";
 import Footer from "../Components/Footer";
 
+import { Button, Flex } from "antd";
+
+
+
+
 export default function PhonesTablets() {
   return (
     <div className="overflowBox">
-      <div>PhonesTablets</div>
+      <div style={{ height: "auto", width: "100%" }}>
+        <div className="phonesTabletsTopBox">
+          <Flex wrap="wrap" gap="small">
+            {Array.from(
+              {
+                length: 24,
+              },
+              (_, i) => (
+                <Button className="phonesTabletsTopBox-Button" shape="round"  >
+                  <p className="phonesTabletsTopBox-ButtonText">Download</p>
+                
+              </Button>
+              )
+            )}
+          </Flex>
+        </div>
+        <div style={{ height: "auto", width: "100%", marginTop:'40px' }}>
+          <div>Left</div>
+          <div>Right</div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
