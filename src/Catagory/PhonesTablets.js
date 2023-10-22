cb1b24487a8bec286da05837e40e8e5fbe834a26
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-23 02:23:18 +0600

    ok

--- src/Catagory/PhonesTablets.js
+++ src/Catagory/PhonesTablets.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import "./PhonesTablets.css";
 import Footer from "../Components/Footer";
+import OffersCard from "../Card/OffersCard";
 
 import { Button, Flex, Drawer, Select } from "antd";
 import { FilterOutlined } from "@ant-design/icons";
 
+import iphone from "../Image/iphone15-pro-64.webp";
+import goPro from "../Image/hero12blackheader.jpeg";
+import s23 from "../Image/samsung s23 ultra.webp";
+import airpodsPro from "../Image/airpods pro.jpeg";
+import macbookPro from "../Image/macbook pro.jpeg";
+import joyroom from "../Image/Joyroom JR-W020.jpeg";
+
 export default function PhonesTablets() {
   const [open, setOpen] = useState(false);
-  const [size, setSize] = useState();
 
   const showDefaultDrawer = () => {
-    setSize("default");
     setOpen(true);
   };
 
@@ -19,7 +25,7 @@ export default function PhonesTablets() {
   };
 
   const handleChange = (value) => {
-    console.log(value); 
+    console.log(value);
   };
   return (
     <div className="overflowBox">
@@ -32,7 +38,7 @@ export default function PhonesTablets() {
               },
               (_, i) => (
                 <Button className="phonesTabletsTopBox-Button" shape="round">
-                  <p className="phonesTabletsTopBox-ButtonText">Download</p>
+                  <p className="phonesTabletsTopBox-ButtonText">Brand Name</p>
                 </Button>
               )
             )}
@@ -48,6 +54,7 @@ export default function PhonesTablets() {
         >
           <div className="phonesTabletsLeftBox">Left</div>
           <div className="phonesTabletsRightBox">
+            {/* Header */}
             <div className="phonesTabletsRight-Header">
               <h3 className="phonesTabletsRight-HeaderText">
                 Phones & Tablets
@@ -102,6 +109,56 @@ export default function PhonesTablets() {
                   ]}
                 />
               </div>
+            </div>
+            {/* Product Demo Box */}
+            <div>
+              <OffersCard
+                name="Iphone 15 Pro"
+                Img={iphone}
+                percentage="14"
+                number="125000"
+              />
+
+              <OffersCard
+                name="Go Pro 12"
+                Img={goPro}
+                percentage="4"
+                number="25000"
+              />
+              <OffersCard
+                name="Macbook Pro 2023"
+                Img={macbookPro}
+                percentage="14"
+                number="250000"
+              />
+              <OffersCard
+                name="Joyroom JR-W020"
+                Img={joyroom}
+                percentage="9"
+                number="2500"
+              />
+              <OffersCard
+                name="Samsung s23 ultra"
+                Img={s23}
+                percentage="25"
+                number="100000"
+              />
+              <OffersCard
+                name="Airpods pro"
+                Img={airpodsPro}
+                percentage="15"
+                number="45000"
+              />
+              <OffersCard />
+              <OffersCard name="Iphone 15 Pro" Img={iphone} />
+              <OffersCard name="Iphone 15 Pro" Img={iphone} percentage="14" />
+              <OffersCard
+                name="Iphone 15 Pro"
+                percentage="14"
+                number="125000"
+              />
+
+              <OffersCard />
             </div>
           </div>
         </div>
