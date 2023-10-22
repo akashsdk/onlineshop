c36c273f15f5bfebc3ac0790fa6dd5b3c7d3bdb0
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-10-23 00:37:03 +0600

    ok

--- src/Catagory/Accessories.js
+++ src/Catagory/Accessories.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Accessories.css';
+import Footer from "../Components/Footer";
 
 export default function Accessories() {
   return (
+    <div className="overflowBox">
     <div>Accessories</div>
+    <Footer />
+  </div>
   )
 }
